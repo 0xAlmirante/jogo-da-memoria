a05190f9568f6d7fc7b15e59140cb87c570fd3ce
Thiago Duda <thiagodudacc@gmail.com>
2019-08-26 17:56:02 -0300

    Desafio aula 021

--- src/index.js
+++ src/index.js
@@ -14,3 +14,19 @@ $memoryCard.classList.add("memory-card");
 $root.insertBefore($memoryCard, null);
 
 $memoryCard.insertAdjacentHTML("afterbegin", $icon);
+
+
+// Parte frontal do card
+const $memoryCardFront = document.createElement("article");
+const $iconFront = `
+  <img
+    src="img/icon-js.png"
+    alt="icon-js"
+    class="icon"
+  />
+`
+$memoryCardFront.classList.add("memory-card", "-front");
+
+$root.insertBefore($memoryCardFront, $memoryCard);
+
+$memoryCardFront.insertAdjacentHTML("afterbegin", $iconFront);
