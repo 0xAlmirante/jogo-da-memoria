001d6d8555ecd31a2cfbe4d5ac79c290489f3092
Thiago Duda <thiagodudacc@gmail.com>
2019-08-26 17:25:02 -0300

    Criado o componente memory card

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const $root = document.querySelector("#root");
+const $memoryCard = document.createElement("article");
+const $icon = `
+  <img 
+    src='img/icon-collabcode.svg'
+    alt='Mascote CollabCode'
+    class='icon'
+  />
+`;
+
+$memoryCard.classList.add("memory-card");
+
+
+$root.insertBefore($memoryCard, null);
+
+$memoryCard.insertAdjacentHTML("afterbegin", $icon);
