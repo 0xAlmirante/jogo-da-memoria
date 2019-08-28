698d66048bde988cc5fce5f35001a12e5cd21c89
Thiago Duda <thiagodudacc@gmail.com>
2019-08-28 09:31:29 -0300

    Desafio Aula 022

--- src/index.js
+++ src/index.js
@@ -1,20 +1,4 @@
 const $root = document.querySelector("#root");
-const $memoryCard = document.createElement("article");
-const $iconCollab = `
-  <img 
-    src='img/icon-collabcode.svg'
-    alt='Mascote CollabCode'
-    class='icon'
-  />
-`;
-
-$memoryCard.classList.add("memory-card");
-
-$root.insertBefore($memoryCard, null);
-
-$memoryCard.insertAdjacentHTML("afterbegin", $iconCollab);
-
-// Parte frontal do card
 const $memoryCardFront = document.createElement("article");
 const $iconJS = `
   <img
@@ -22,9 +6,24 @@ const $iconJS = `
     alt="icon-js"
     class="icon"
   />
-`
+`;
+const maxCards = 9;
+
 $memoryCardFront.classList.add("memory-card", "-front");
-
-$root.insertBefore($memoryCardFront, $memoryCard);
-
+$root.insertBefore($memoryCardFront, null);
 $memoryCardFront.insertAdjacentHTML("afterbegin", $iconJS);
+
+// Desafio
+for(let i = 0; i < maxCards; i++){
+  const $memoryCard = document.createElement("article");
+  const $iconCollab = `
+  <img 
+    src='img/icon-collabcode.svg'
+    alt='Mascote CollabCode'
+    class='icon'
+  />
+`;
+  $memoryCard.classList.add("memory-card");
+  $root.insertBefore($memoryCard, null);
+  $memoryCard.insertAdjacentHTML("afterbegin", $iconCollab);
+}
