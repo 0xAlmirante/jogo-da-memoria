be0b3ce4a6d4caac98286653b7ab137944478550
Thiago Duda <thiagodudacc@gmail.com>
2019-09-03 08:30:31 -0300

    Desafio Aula 026

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -9,3 +9,49 @@ const createCard = ({cardClass ,src, alt}) => `
 `;
 
 const  handleClick = () => console.log("Ae");
+
+window.onload = () => {
+  const $head =  document.querySelector("head");
+  const $style = document.createElement("style");
+  
+  $style.textContent = `
+    .memory-card{
+      width: 150px;
+      height: 150px;
+      background-color: #f25a70;
+      border-radius: 30px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+      cursor: pointer;
+    }
+  
+    .memory-card:active{
+      transform: scale(.9);
+    }
+  
+    .memory-card.-front{
+      background-color: #fff;
+    }
+  
+    .memory-card > .icon{
+      width: 100px;
+      height: 100px;
+    }
+  
+    .memory-card.-front > .icon{
+      position: absolute;
+      transform: translateY(-10px);
+    }
+  
+    .memory-card.-front::before{
+      content: "";
+      width: 95px; 
+      height: 95px;
+      background-color: #d4d4d4;
+      border-radius: 50%;
+    }
+  `
+  $head.insertBefore($style, null);
+}
