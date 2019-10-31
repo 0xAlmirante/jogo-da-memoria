0944c4f052611580544cffa1c5e2b2fbb37acf63
Thiago Duda <thiagodudacc@gmail.com>
2019-10-31 14:42:28 -0300

    Refactor: Aula 039

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,9 +1,9 @@
 (function() {
     const $root = document.querySelector("#root");
 
-    const $flatButton = flatButton.render("Log in");
-    const $signUpButton = flatButton.render("Sign Up", "-signup");
+    const $loginButton = flatButton.render("Log in");
+    const $signUpButton = flatButton.render("Sign Up", true);
 
-    $root.insertAdjacentHTML("beforeend", $flatButton);
+    $root.insertAdjacentHTML("beforeend", $loginButton);
     $root.insertAdjacentHTML("beforeend", $signUpButton)
 })();
